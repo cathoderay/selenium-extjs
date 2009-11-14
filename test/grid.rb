ff2006dbd2efd94ecd29964a1d8fe9b7cd78109c
jlbfalcao <jlbfalcao@gmail.com>
2009-11-14 20:07:12 -0200

    cleaning grid.rb test

--- test/grid.rb
+++ test/grid.rb
@@ -13,30 +13,16 @@ begin
 
   # wait for window.
   window = Ext::find(:xtype => 'window', :wait => true)
+  # close the window
   window.close
-  
-  sleep 2
 
-  # search for appfeedgrid component.
+  # search for editorgrid component.
   editorgrid = Ext::find(:xtype => "editorgrid")
 
-  
-  # window.Ext.ComponentMgr.all.find(function(el){ return el.getXType() == 'window' })
-
-  print " >>> "
-  p editorgrid
-  editorgrid.highlight
-
+  # set row 1 with data
   editorgrid.edit_row(1, ["Jorge", "Shade", "10.10", "24/03/06", true]);
 
   sleep 10
-# Ext.getCmp("ext-comp-1005").store.reload()
-
-  # return button.
-  # button = Ext::find(:xtype => "button", :text => 'Open All', :xparent => appfeedgrid)
-  # button.click
-  # 
-  # sleep 10
 
 ensure
   s.close_current_browser_session    
