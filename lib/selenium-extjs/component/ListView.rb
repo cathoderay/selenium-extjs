2a63746d848456dac168d654d19a6c4c8870e7f2
Ronald Andreu Kaiser <raios.catodicos@gmail.com>
2010-03-24 17:02:14 -0300

    wait_for_row_visible method for listviews

--- lib/selenium-extjs/component/ListView.rb
+++ lib/selenium-extjs/component/ListView.rb
@@ -3,5 +3,18 @@ module Ext
         def click_at_row(label)
             @selenium.click_at(node() + "//div[contains(@class, 'x-list-body-inner')]//em[text() = '#{label}']", "0,0")
         end
+
+        def wait_for_row_visible(label, timeout=10)
+          exp = node() + "//div[contains(@class, 'x-list-body-inner')]//em[text() = '#{label}']"
+                t0 = Time.now
+                while true
+                  begin
+                    return true if @selenium.is_visible(exp)
+                  rescue
+                  end
+                  return false if (Time.now - t0) > timeout
+                end
+        end
+
     end
 end
--- test/list_view_test.rb
+++ test/list_view_test.rb
@@ -3,16 +3,16 @@ require 'test/unit'
 require 'selenium-extjs'
 require 'setup'
 
-class ComboBoxTest < Test::Unit::TestCase
+class ListViewTest < Test::Unit::TestCase
 
   include Setup
 
   def test_list_view
-    @s.open 'deploy/dev/examples/view/list-view.html'
-    sleep 3
+    @s.open 'deploy/dev/examples/view/list-view.html'    
     listview = @s.find_ext(:xtype => "listview", :wait => true)
+
     fail if not listview.is_a? Ext::ListView
-    
+    listview.wait_for_row_visible('sara_pink.jpg')    
     listview.click_at_row('sara_pink.jpg')
   end
 end
