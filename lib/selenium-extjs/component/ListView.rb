59bd04513b83a5a7263dbc58638ce4fe2f5decb0
Ronald Andreu Kaiser <raios.catodicos@gmail.com>
2010-03-24 17:18:02 -0300

    has_row method for listviews

--- lib/selenium-extjs/component/ListView.rb
+++ lib/selenium-extjs/component/ListView.rb
@@ -1,5 +1,10 @@
 module Ext
     class ListView < Component
+        
+        def has_row(label)
+            @selenium.is_element_present(node() + "//div[contains(@class, 'x-list-body-inner')]//em[text() = '#{label}']") 
+        end
+
         def click_at_row(label)
             @selenium.click_at(node() + "//div[contains(@class, 'x-list-body-inner')]//em[text() = '#{label}']", "0,0")
         end
--- test/list_view_test.rb
+++ test/list_view_test.rb
@@ -10,9 +10,9 @@ class ListViewTest < Test::Unit::TestCase
   def test_list_view
     @s.open 'deploy/dev/examples/view/list-view.html'    
     listview = @s.find_ext(:xtype => "listview", :wait => true)
-
     fail if not listview.is_a? Ext::ListView
     listview.wait_for_row_visible('sara_pink.jpg')    
+    p listview.has_row('sara_pink.jpg')
     listview.click_at_row('sara_pink.jpg')
   end
 end
