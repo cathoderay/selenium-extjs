8f514d7b8fe2f4a74338978d1db9fee3366fa02a
Ronald Andreu Kaiser <raios.catodicos@gmail.com>
2010-05-03 20:26:41 -0300

    implementing methods for dataview...

--- lib/selenium-extjs/component/DataView.rb
+++ lib/selenium-extjs/component/DataView.rb
@@ -4,5 +4,14 @@ module Ext
 			#TODO: generalize this (h4)
         	#@selenium.click_at(node() + "//div[contains(@class, 'x-list-body-inner')]//h4[contains(text(), '" + label + "')]", "0,0")
 		end
+
+        def get_item_selector()
+            selector  = @selenium.get_eval("window.Ext.getCmp('#{@id}').itemSelector") 
+            return node() + "//" + selector.split(".")[0]
+        end
+
+        def is_text_present(path, text)
+            return @selenium.is_visible(path + "//*[contains(text(), '#{text}')]")
+        end
 	end
 end
